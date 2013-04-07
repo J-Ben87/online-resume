234cf74ded0f88d405572767053ce82baaed9dfa
Benoit Jouhaud <bjouhaud@gmail.com>
2013-04-08 03:42:00 +1000

    temporary commit to create the detail entry

--- routes/api/detail.js
+++ routes/api/detail.js
@@ -16,6 +16,32 @@
       });
     });
 
+    app.post('/api/detail', passport.authenticate('bearer', { session: false }), function(req, res) {
+      var detail = new DetailModel({
+        name: req.body.name,
+        title: req.body.title,
+        subtitle: req.body.subtitle,
+        phone: req.body.phone,
+        email: req.body.email,
+        address: req.body.address,
+        culture: req.body.culture,
+        country: req.body.country,
+        flag: req.body.flag,
+        visa: req.body.visa,
+        driving_license: req.body.driving_license
+      });
+
+      detail.save(function(err) {
+        if (!err) {
+          return console.log('Detail created');
+        } else {
+          return console.log(err);
+        }
+      });
+
+      return res.send(detail);
+    });
+
     app.put('/api/detail', passport.authenticate('bearer', { session: false }), function(req, res) {
       return DetailModel.findOne(function(err, detail) {
         detail.name = req.body.name;
